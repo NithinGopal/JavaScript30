05f7516bc0d37ca0767568e74d3f6be44ac679f7
Nithin Gopal <nithingopal2015@gmail.com>
2022-03-25 23:15:05 +0530

    Add external js file
    - keypress detection function added
    - buttonanimation func added
    - playsound function added

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,38 @@
+// Detecting Keypress
+
+document.addEventListener("keydown", function (event){
+    buttonAnimation(event.key.toUpperCase());
+    playSound(event.key.toLocaleUpperCase());
+})
+
+//Detecting Button Press
+
+// for (i=0; i<document.querySelectorAll('.key').length; i++) {
+//     document.querySelectorAll('.key')[i].addEventListener("click", function ({
+//         let buttonClicked = this.querySelector("." + )
+//         console.log(buttonClicked);
+//     }))
+// }
+
+//Button Animation
+
+function buttonAnimation(currentkey){
+    let activeButton = document.querySelector("." + currentkey);
+    //console.log(activeButton);
+    activeButton.classList.add('playing');
+    
+    setTimeout(() => {
+        activeButton.classList.remove('playing');
+    },100);
+}
+
+// Playing Sound 
+
+function playSound(keyPressed){
+    //console.log(keyPressed);
+    let keyAttr = document.querySelector("." + keyPressed).getAttribute('data-key');
+    //console.log(keyAttr);
+    let sound = document.querySelector(`audio[data-key = "${keyAttr}"]`);
+    //console.log(sound);
+    sound.play();
+}
